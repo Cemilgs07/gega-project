b251ac77656068a6a76d60e43968091c264685c1
Cemil KUTLUCA <39375588+Cemilgs07@users.noreply.github.com>
2024-12-30 03:23:52 +0300

    Navbar yapimi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "serif"],
+        open: ["Open Sans", "serif"],
+      },
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD161",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#fffefd",
+      },
+    },
   },
   plugins: [],
 };
